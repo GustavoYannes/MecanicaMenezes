d7eedac34694a599140f41bca425689b807164fc
Andre <andreaugustosilvacarvalho@gmail.com>
2026-06-07 21:48:37 -0300

    Feat: Ajuste de build

--- code/backoficina/src/main/java/com/oficinaMenezes/backoficina/infra/config/WebConfig.java
+++ code/backoficina/src/main/java/com/oficinaMenezes/backoficina/infra/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.oficinaMenezes.backoficina.infra.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,10 +10,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "http://192.168.18.5:4200")
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
